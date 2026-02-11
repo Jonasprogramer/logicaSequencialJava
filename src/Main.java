815eb75b87edbe225ba8253ad305e6395d111b52
jonas Miqueias <jhonasbola2@gmail.com>
2026-02-10 21:49:26 -0300

    ex001

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("ola");
+
+       Scanner rd = new Scanner(System.in);
+       int n1;
+       int n2;
+
+       n1 = rd.nextInt();
+       n2 = rd.nextInt();
+
+       int sum = n1 + n2;
+
+       System.out.println("SOMA = " + sum);
     }
 }
